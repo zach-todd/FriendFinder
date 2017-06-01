ac88387d354db7d307be5d02a286597b25b096e3
Zach Todd <gingergoalie@gmail.com>
2017-06-01 16:50:45 -0400

    working local

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friends = [
+{
+  "name":"Ahmed",
+  "photo":"https://tse4.mm.bing.net/th?id=ORT.TH_470633631&pid=1.12&eid=G.470633631",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+];
+
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,62 +1,51 @@
-  // Capture the form inputs 
-    $("#submit").on("click", function(){
 
-    	// Form validation
-    	function validateForm() {
-		  var isValid = true;
-		  $('.form-control').each(function() {
-		    if ( $(this).val() === '' )
-		        isValid = false;
-		  });
+var friends = require("../data/friends");
 
-		  $('.chosen-select').each(function() {
+module.exports = function(app) {
 
-		  	if( $(this).val() === "")
-		  		isValid = false
-		  })
-		  return isValid;
-		}
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
 
-		// If all required fields are filled
-		if (validateForm() == true)
-		{
-			// Create an object for the user's data
-	    	var userData = {
-	    		name: $("#name").val(),
-	    		photo: $("#photo").val(),
-	    		scores: [$("#q1").val(), 
-	    		$("#q2").val(), 
-	    		$("#q3").val(), 
-	    		$("#q4").val(),
-	    		$("#q5").val(), 
-	    		$("#q6").val(), 
-	    		$("#q7").val(), 
-	    		$("#q8").val(), 
-	    		$("#q9").val(), 
-	    		$("#q10").val(), ]
-	    	}
+  app.post("/api/friends", function(req, res) {
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
 
+   
+    var userData = req.body;
+    var userScores = userData.scores;
 
-	    	// Grab the URL of the website
-	    	var currentURL = window.location.origin;
+   
+    var totalDifference = 0;
 
-	    	// AJAX post the data to the friends API. 
-	    	$.post(currentURL + "../data/friends", userData, function(data){
+    
+    for (var i = 0; i < friends.length; i++) {
 
-	    		// Grab the result from the AJAX post so that the best match's name and photo are displayed.
-	    		$("#matchName").text(data.name);
-	    		$('#matchImg').attr("src", data.photo);
+      console.log(friends[i].name);
+      totalDifference = 0;
 
-		    	// Show the modal with the best match 
-		    	$("#resultsModal").modal('toggle');
+      
+      for (var j = 0; j < friends[i].scores[j]; j++) {
 
-	    	});
-		}
-		else
-		{
-			alert("Please fill out all fields before submitting!");
-		}
-    	
-    	return false;
-    });
+       
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
 
+        
+        if (totalDifference <= bestMatch.friendDifference) {
+
+          
+          bestMatch.name = friends[i].name;
+          bestMatch.photo = friends[i].photo;
+          bestMatch.friendDifference = totalDifference;
+        }
+      }
+    }
+    friends.push(userData);
+    res.json(bestMatch);
+
+  });
+
+};
