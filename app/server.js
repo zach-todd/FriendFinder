6ce238d0f5d3f238ed8eb94043059858c0219b70
Zach Todd <gingergoalie@gmail.com>
2017-06-04 20:21:43 -0400

    working local

--- app/server.js
+++ app/server.js
@@ -2,15 +2,15 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
-var PORT = 8080;
+app.set('port', (process.env.PORT || 8080));
 
 	app.use(bodyParser.json());
 	app.use( bodyParser.urlencoded( {extended:true}));
-	
+
 	require("./routing/apiRoutes.js")(app);
 	require("./routing/htmlRoutes.js")(app);
 	
 
-	app.listen(PORT, function(){
-		console.log("app listening on " + PORT);
+	app.listen(app.get('port'), function(){
+		console.log("app listening on " , app.get('port'));
 	})
